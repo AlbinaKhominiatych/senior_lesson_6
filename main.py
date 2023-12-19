184b823742d5a58f475342682167128cb592ec2c
khominiatych <hominyatichalbina@gmail.com>
2023-12-19 17:14:11 +0200

    raise

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ try:
     number = int(input("Введіть число:"))
 except ValueError as e:
     print(e)
+
+#підняття винятків
+text = input("Введіть текст! ")
+if type(text) != int:
+    raise TypeError(f"Ми не працюємо з цим {type(text)} типом")
