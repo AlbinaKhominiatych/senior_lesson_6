a1ecde8c2ead375f2d49ffa474d84f81b54246c2
khominiatych <hominyatichalbina@gmail.com>
2023-12-19 16:58:17 +0200

    ще приклад використання

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ except ZeroDivisionError as e:
     print(f"Ділити на нуль не можна! {e}")
 finally:
     print("Далі якийсь код")
+
+try:
+    number = int(input("Введіть число:"))
+except ValueError as e:
+    print(e)
